f1ecd5522000c053964146c6706960ea8333fd4a
estebanjosedemarchi@gmail.com <estebanjosedemarchi@gmail.com>
2022-10-08 22:14:02 -0300

    update expects an Employee

--- src/domain/repositories/EmployeeRepository.ts
+++ src/domain/repositories/EmployeeRepository.ts
@@ -1,11 +1,10 @@
 import {Employee} from "../entities/Employee";
-import {EmployeeUpdate} from "../entities/EmployeeUpdate";
 
 export interface EmployeeRepository {
   getAll: () => Promise<Employee[]>
   save: (employee: Employee) => Promise<Employee>
   getByUsername: (username: String) => Promise<Employee | null>
   getById: (id: string | number) => Promise<Employee | null>
-  update: (employeeUpdate: EmployeeUpdate) => Promise<Employee>
+  update: (employee: Employee) => Promise<Employee>
   deleteById: (id: string | number) => Promise<void>
 }
